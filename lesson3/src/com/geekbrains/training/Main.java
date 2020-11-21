c8eee88be186ccf46945d72b9b6ce2134864d48b
Egor Klepikov <Egor.Klepikov@jetbrains.com>
2020-11-21 21:59:49 +0300

    Implemented select game algorithm

--- lesson3/src/com/geekbrains/training/Main.java
+++ lesson3/src/com/geekbrains/training/Main.java
@@ -1,7 +1,34 @@
 package com.geekbrains.training;
 
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    boolean isGameSelected = false;
+    while (!isGameSelected) {
+      System.out.print("Enter game name: ");
+      switch (scanner.nextLine().toLowerCase()) {
+        case "guess number":
+          isGameSelected = true;
+          playGuessNumber();
+          break;
+        case "guess word":
+          isGameSelected = true;
+          playGuessWord();
+          break;
+        default:
+          System.out.println("Game name is wrong. Press enter to try again.");
+          System.out.println("-------------------------------------------------");
+      }
+    }
+  }
+
+  private static void playGuessNumber() {
+
+  }
+
+  private static void playGuessWord() {
 
   }
 }
