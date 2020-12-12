1251d665da6a548e970f261ca59739e98937a951
Egor Klepikov <Egor.Klepikov@jetbrains.com>
2020-12-12 23:17:47 +0300

    resolved paths issue

--- lesson8/src/com/geekbrains/training/ui/game/Cross.java
+++ lesson8/src/com/geekbrains/training/ui/game/Cross.java
@@ -9,7 +9,7 @@ public class Cross extends GameElement {
   public Cross(int cellCoordinateX, int cellCoordinateY) {
     super(cellCoordinateX, cellCoordinateY);
     try {
-      image = ImageIO.read(new File("C:\\Users\\egork\\IdeaProjects\\GeekBrains - practice\\lesson8\\src\\cross.png"));
+      image = ImageIO.read(new File("lesson8\\assets\\cross.png"));
     } catch (IOException e) {
       e.printStackTrace();
     }
--- lesson8/src/com/geekbrains/training/ui/game/Zero.java
+++ lesson8/src/com/geekbrains/training/ui/game/Zero.java
@@ -8,7 +8,8 @@ public class Zero extends GameElement {
   public Zero(int cellCoordinateX, int cellCoordinateY) {
     super(cellCoordinateX, cellCoordinateY);
     try {
-      image = ImageIO.read(new File("C:\\Users\\egork\\IdeaProjects\\GeekBrains - practice\\lesson8\\src\\zero.png"));
+      image = ImageIO.read(new File("lesson8\\assets\\zero.png"));
+      //C:\Users\egork\IdeaProjects\GeekBrains - practice\lesson8\assets
     } catch (IOException e) {
       e.printStackTrace();
     }
