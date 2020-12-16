d322965d811c164af03ca8c5d5095dbd51b25a1b
Egor Klepikov <Egor.Klepikov@jetbrains.com>
2020-12-16 23:02:39 +0300

    Implemented task number 3

--- lesson9/src/com/geekbrains/practice/entry/Main.java
+++ lesson9/src/com/geekbrains/practice/entry/Main.java
@@ -1,4 +1,32 @@
 package com.geekbrains.practice.entry;
 
+import com.geekbrains.practice.creatures.Cat;
+import com.geekbrains.practice.creatures.Human;
+import com.geekbrains.practice.creatures.ICreature;
+import com.geekbrains.practice.creatures.Robot;
+import com.geekbrains.practice.elements.IBarrier;
+import com.geekbrains.practice.elements.RunningTrack;
+import com.geekbrains.practice.elements.Wall;
+
 public class Main {
+  public static void main(String[] args) {
+    ICreature[] creatures = {
+      new Cat(),
+      new Human(),
+      new Robot()
+    };
+    IBarrier[] barriers = {
+      new Wall(),
+      new RunningTrack()
+    };
+    startChallenge(creatures, barriers);
+  }
+
+  private static void startChallenge(ICreature[] creatures, IBarrier[] barriers) {
+    for (ICreature creature : creatures) {
+      for (IBarrier barrier : barriers) {
+        barrier.performAction(creature);
+      }
+    }
+  }
 }
