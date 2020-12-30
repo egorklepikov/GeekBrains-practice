3aa52bf7a8c0c72f8a2066fac3875a1a54de4a40
Egor Klepikov <Egor.Klepikov@jetbrains.com>
2020-12-30 19:38:12 +0300

    Added onMousePressedTopPanel and onMouseDraggedTopPanel on top panel

--- lesson12/src/main/java/com/geekbrains/practice/ui/ChatController.java
+++ lesson12/src/main/java/com/geekbrains/practice/ui/ChatController.java
@@ -8,11 +8,15 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ChatController implements Initializable {
+  @FXML
+  private GridPane mainPane;
   @FXML
   private ImageView closeButton;
   @FXML
@@ -21,6 +25,9 @@ public class ChatController implements Initializable {
   private TextArea messagesArea;
   @FXML
   private TextField inputMessageField;
+
+  private double xOffset;
+  private double yOffset;
 
   @FXML
   @Override
@@ -61,17 +68,30 @@ public class ChatController implements Initializable {
     inputMessageField.requestFocus();
   }
 
+  @FXML
   public void onMouseEnteredExit() {
     closeButton.setScaleX(1.1);
     closeButton.setScaleY(1.1);
   }
 
+  @FXML
   public void onMouseExitedExit() {
     closeButton.setScaleX(1.0);
     closeButton.setScaleY(1.0);
   }
 
+  @FXML
   public void onMouseClickedExit() {
     System.exit(0);
+  }
+
+  public void onMousePressedTopPanel(MouseEvent mouseEvent) {
+    xOffset = mainPane.getScene().getWindow().getX() - mouseEvent.getScreenX();
+    yOffset = mainPane.getScene().getWindow().getY() - mouseEvent.getScreenY();
+  }
+
+  public void onMouseDraggedTopPanel(MouseEvent mouseEvent) {
+    mainPane.getScene().getWindow().setX(mouseEvent.getScreenX() + xOffset);
+    mainPane.getScene().getWindow().setY(mouseEvent.getScreenY() + yOffset);
   }
 }
