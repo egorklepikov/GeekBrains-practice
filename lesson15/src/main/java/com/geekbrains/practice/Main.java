3d75d33013d0f0a4869ffae72d748ff2fb290280
Egor Klepikov <Egor.Klepikov@jetbrains.com>
2021-01-27 22:29:29 +0300

    Implemented task number 2

--- lesson15/src/main/java/com/geekbrains/practice/Main.java
+++ lesson15/src/main/java/com/geekbrains/practice/Main.java
@@ -1,12 +1,17 @@
 package com.geekbrains.practice;
 
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
   public static void main(String[] args) {
     Integer[] array = new Integer[]{1, 2, 3};
     System.out.println(Arrays.toString(swapElements(array, 0, 1)));
+
+    ArrayList<Object> convertedArray = castArrayToArrayList(new Object[]{1,2,3});
+    convertedArray.add(4);
+    System.out.println(convertedArray);
   }
 
   private static <T> T[] swapElements(T[] array, int first, int second) {
@@ -14,5 +19,9 @@ public class Main {
     array[first] = array[second];
     array[second] = tmpElement;
     return array;
+  }
+
+  private static ArrayList<Object> castArrayToArrayList(Object[] array) {
+    return new ArrayList<>(Arrays.asList(array));
   }
 }
