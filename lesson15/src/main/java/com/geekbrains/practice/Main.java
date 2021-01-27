9efd5915bf996937b88804f5097497a882debf81
Egor Klepikov <Egor.Klepikov@jetbrains.com>
2021-01-27 22:23:11 +0300

    Implemented task number 1

--- lesson15/src/main/java/com/geekbrains/practice/Main.java
+++ lesson15/src/main/java/com/geekbrains/practice/Main.java
@@ -1,7 +1,18 @@
 package com.geekbrains.practice;
 
+
+import java.util.Arrays;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("initial");
+    Integer[] array = new Integer[]{1, 2, 3};
+    System.out.println(Arrays.toString(swapElements(array, 0, 1)));
+  }
+
+  private static <T> T[] swapElements(T[] array, int first, int second) {
+    T tmpElement = array[first];
+    array[first] = array[second];
+    array[second] = tmpElement;
+    return array;
   }
 }
