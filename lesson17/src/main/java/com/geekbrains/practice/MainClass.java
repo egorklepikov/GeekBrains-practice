c3bc5d2a2288305014646e48bf10f645ed49633b
egorklepikov <egor.klepickov95@yandex.ru>
2021-02-13 15:46:56 +0300

    Blocked main thread until all cars are finished

--- lesson17/src/main/java/com/geekbrains/practice/MainClass.java
+++ lesson17/src/main/java/com/geekbrains/practice/MainClass.java
@@ -1,5 +1,6 @@
 package com.geekbrains.practice;
 
+import java.util.ArrayList;
 import java.util.concurrent.CountDownLatch;
 
 public class MainClass {
@@ -9,12 +10,16 @@ public class MainClass {
     System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
     Race race = new Race(new Road(60), new Tunnel(), new Road(40));
     CountDownLatch countDownLatch = new CountDownLatch(CARS_COUNT);
+
     Car[] cars = new Car[CARS_COUNT];
     for (int i = 0; i < cars.length; i++) {
       cars[i] = new Car(race, 20 + (int) (Math.random() * 10), countDownLatch);
     }
+    ArrayList<Thread> carsList = new ArrayList<>();
     for (int i = 0; i < cars.length; i++) {
-      new Thread(cars[i]).start();
+      Thread carThread = new Thread(cars[i]);
+      carsList.add(carThread);
+      carThread.start();
     }
     try {
       countDownLatch.await();
@@ -22,6 +27,13 @@ public class MainClass {
       e.printStackTrace();
     }
     System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+    for (Thread car : carsList) {
+      try {
+        car.join();
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
     System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
   }
 }
